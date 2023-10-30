51276959a44eec9601e68c716232c8718d8d9627
Kelvin Johnson <kelvin.johnson@d18sg.com>
2023-10-30 17:23:42 -0500

    initial project creation

---
+++ src/main/java/com/d18sg/flowctrl/FlowctrlApplication.java
@@ -0,0 +1,13 @@
+package com.d18sg.flowctrl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowctrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowctrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/d18sg/flowctrl/FlowctrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d18sg.flowctrl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowctrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
